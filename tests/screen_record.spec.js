88497f71d6386af3af18517afc441857d02ace04
Ganesh-186 <ganeshravichandran.06@gmail.com>
2026-06-02 18:58:41 +0530

    Removed screen record code

--- tests/screen_record.spec.js
+++ tests/screen_record.spec.js
@@ -1,15 +1,1 @@
-import test from '@playwright/test'
 
-test('screen recording',async({browser})=>{
-    const context1 = await browser.newContext({
-        recordVideo:{
-            dir:'recordvideo/'
-        }
-    });
-    const page1 = await context1.newPage();
-    await page1.goto('https://www.amazon.in/');
-
-    const page2 = await context1.newPage();
-    await page2.goto('https://www.flipkart.com/');
-
-})
